2bf4d42df84a1fa33614647a8e28d2e15773058a
Juan Montoya <72569343+J-Mont07@users.noreply.github.com>
2020-12-10 21:23:59 -0600

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -16,43 +16,30 @@ player = mc.main_character()
 #Intro text to the user
 #The empty input functions are only here so the player has to type something to move on
 #(like pressing a button in a video game to move the dialogue along)
-##print("WELCOME TO NECROSHEEP!")
-##input (e)
-##user = input("Are you ready for this challenge? [Y/N]" +"\n")
+print("WELCOME TO NECROSHEEP!")
+input (e)
+user = input("Are you ready for this challenge? [Y/N]" +"\n")
 ##
-##while user.upper() != 'Y' and user.upper() != 'N':
-##    print ("Try again...")
-##    user = input ("Are you ready for this challange? [Y/N]" + "\n")
-##if user.upper() == "Y":
-##    print ("Let the game begin")
-##    sec_one.start(player)
+while user.upper() != 'Y' and user.upper() != 'N':
+    print ("Try again...")
+    user = input ("Are you ready for this challange? [Y/N]" + "\n")
+if user.upper() == "Y":
+    print ("Let the game begin")
+    sec_one.start(player)
 ##
-##elif user.upper() == "N":
-##    print ("HAHAHA I knew you weren't up for the challange" )
+elif user.upper() == "N":
+    print ("HAHAHA I knew you weren't up for the challange" )
 ##
-###once the player finishes the first level the second level will execute.
-##
-###This code will run chapter two
-##print("You have completed the first challance.")
-##input (e)
-##print("Lets continue your Journey...")
-##sec_two.start(player)
+once the player finishes the first level the second level will execute.
+
+This code will run chapter two
+print("You have completed the first challance.")
+input (e)
+print("Lets continue your Journey...")
+sec_two.start(player)
 
 #This code will run chapter three
 print("Amazing!! This journy hasn't been that dificult for you.")
 input(e)
 print ("Keep up that great work!")
 sec_three.start(player)
-
-#if user.upper() == "Y":
-#    print ("Let the game begin")
-#    Section_one.start(player)
-#elif user.upper() == "N":
-#    print ("HAHAHA I knew you weren't up for the challange")
-#else:
-#    print ("Try again...")
-    
-#And do/say anything else to get your game started
-
-#Then call your first section.
-#Be sure to pass in your player character
